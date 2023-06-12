0bf32ae54771f577f08e6bbc65fe822c4b59023c
Matheus Willian <matheuswill6663@gmail.com>
2023-06-12 15:12:24 -0300

    chore: create connection to SQL and create Models Category and Product

---
+++ VShop.ProductApi/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace VShop.ProductApi.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? ExternalId { get; set; }
+    public string? Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    public ICollection<Product>? Products { get; set; }
+}
---
+++ VShop.ProductApi/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace VShop.ProductApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? ExternalId { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public long Stock { get; set; }
+    public string? ImageURL { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set;}
+    public DateTime? DeletedAt { get; set; }
+
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
+
+}
