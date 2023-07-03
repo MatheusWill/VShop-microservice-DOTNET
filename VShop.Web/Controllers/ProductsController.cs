3e5e50092f6478518e4b1fa380f44949696f692b
Matheus Willian <matheuswill6663@gmail.com>
2023-07-03 17:39:23 -0300

    chore: create CategoryService

--- VShop.Web/Controllers/ProductsController.cs
+++ VShop.Web/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using VShop.Web.Models;
 using VShop.Web.Services.Interfaces;
 
@@ -7,12 +8,15 @@ namespace VShop.Web.Controllers;
 public class ProductsController : Controller
 {
     private readonly IProductService _productService;
+    private readonly ICategoryService _categoryService;
 
     public ProductsController(IProductService productService)
     {
         _productService = productService;
+        _categoryService = categoryService;
     }
 
+    [HttpGet]
     public async Task<ActionResult<IEnumerable<ProductViewModel>>> Index()
     {
         var result = await _productService.GetAllProducts();
@@ -20,5 +24,14 @@ public class ProductsController : Controller
         if (result is null) return View("Error");
 
         return View(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CreateProduct()
+    {
+        ViewBag.CategoryId = new SelectList(await
+             _categoryService.GetAllCategories(), "CategoryId", "Name");
+
+        return View();
     }
 }
---
+++ VShop.Web/Services/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using VShop.Web.Models;
+using VShop.Web.Services.Interfaces;
+
+namespace VShop.Web.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly IHttpClientFactory _httpClient;
+    private readonly JsonSerializerOptions _options;
+    private const string apiEndpoint = "/api/categories/";
+    private const string HTTP_CLIENT_PRODUCT_API = "ProductApi";
+
+    public CategoryService(IHttpClientFactory clientFactory)
+    {
+        _httpClient = clientFactory;
+        _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+    public async Task<IEnumerable<CategoryViewModel>> GetAllCategories()
+    {
+        var client = _httpClient.CreateClient(HTTP_CLIENT_PRODUCT_API);
+
+        IEnumerable<CategoryViewModel> categories;
+
+        var response = await client.GetAsync(apiEndpoint);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        var apiResponse = await response.Content.ReadAsStreamAsync();
+        categories = await JsonSerializer
+            .DeserializeAsync<IEnumerable<CategoryViewModel>>(apiResponse, _options);
+
+        return categories;
+    }
+}
