9d72a766ce2a1779e7c38bb1bf8d96e71f95a22e
Matheus Willian <matheuswill6663@gmail.com>
2023-07-03 17:49:06 -0300

    chore: create View CreateProducts

--- VShop.Web/Models/ProductViewModel.cs
+++ VShop.Web/Models/ProductViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace VShop.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VShop.Web.Models;
 
 public class ProductViewModel
 {
@@ -6,19 +8,25 @@ public class ProductViewModel
 
     public string? ExternalId { get; set; }
 
+    [Required]
     public string? Name { get; set; }
 
+    [Required]
     public decimal Price { get; set; }
 
+    [Required]
     public string? Description { get; set; }
 
+    [Required]
     public long Stock { get; set; }
 
+    [Required]
     public string? ImageURL { get; set; }
 
     public DateTime CreatedAt { get; set; }
 
     public string? CategoryName { get; set; }
 
+    //[Display(Name = "Categorias")]
     public int CategoryId { get; set; }
 }
